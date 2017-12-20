835e8b76636394fffc14211ea930924362d93a4c
wurahara <wurahara@126.com>
2017-12-20 20:36:50 +0800

    finish SMS

---
+++ make.bat
@@ -0,0 +1,16 @@
+xelatex SEU_BeamerTemplate &
+xelatex SEU_BeamerTemplate
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lol /s
+del *.lot /s
+del *.fdb_latexmk /s
+del *.synctex.gz /s
