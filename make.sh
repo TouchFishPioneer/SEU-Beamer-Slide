1b59b3624c995066389d803d57e4909891694362
wurahara <wurahara@126.com>
2017-12-20 21:10:15 +0800

    Mac OS adaptaion

---
+++ make.sh
@@ -0,0 +1,16 @@
+xelatex SEU_BeamerTemplate &
+xelatex SEU_BeamerTemplate
+rm *.aux /s
+rm *.bak /s
+rm *.log /s
+rm *.bbl /s
+rm *.dvi /s
+rm *.blg /s
+rm *.thm /s
+rm *.toc /s
+rm *.out /s
+rm *.lof /s
+rm *.lol /s
+rm *.lot /s
+rm *.fdb_latexmk /s
+rm *.synctex.gz /s
